e4d89e0c59abb3d267c27393689325677e10c743
andrew-yip <andrew.yip22@gmail.com>
2021-08-06 12:38:42 -1000

    added a function to find the empty spaces on the sudoku board (0's)

--- game.py
+++ game.py
@@ -27,3 +27,7 @@ def print_board(board):
 
 
 def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
