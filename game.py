5188bd2470f5ccc6314ba8e8864e3988e61bbd66
andrew-yip <andrew.yip22@gmail.com>
2021-08-07 00:04:43 -1000

    implemented backtracking algorithm to solve and fill the sudoku board

--- game.py
+++ game.py
@@ -11,6 +11,54 @@ board = [
 ]
 
 
+def solve(board):
+    find = find_empty(board)  # to find the next empty spot on the board
+    if not find:
+        return True
+    else:
+        # if we find an empty space we set a tuple to that location and search that row and column
+        row, column = find  # the location of the 0 that was found
+
+    # filling the board with possible solutions
+    for i in range(1, 10):
+        if valid(board, i, (row, column)):
+            board[row][column] = i
+
+            # recursive calls
+            if solve(board):
+                return True
+
+            # backtracking process
+            board[row][column] = 0
+
+    return False
+
+
+def valid(board, number, position):
+    # check row by going through each column (left to right)
+    # we are checking to see if any of the numbers in the current row are the same to the one we just added in
+    for i in range(len(board[0])):  # always will be 9
+        # as long as this isn't the column we just input
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # check column by going through each row (up to down)
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # check 3x3 box to see if it is valid
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
+
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -30,4 +78,18 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
-                return (i, j)  # row, col
+                return (i, j)  # row, col (y, x)
+    return None
+
+
+# Driver Code
+print("------------------------")
+print("Pre Solved Board: ")
+print("------------------------")
+print_board(board)
+solve(board)
+print("------------------------")
+print("Solved Board: ")
+print("------------------------")
+print_board(board)
+print("------------------------")
